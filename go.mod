0047a8d5b1ad629a02804923895565ee44a66dae
cckn <pds@enitt.co.kr>
2020-10-22 18:23:06 +0900

    모드버스 인터페이스 개발 중

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-zone-controller
 
 go 1.14
+
+require (
+	github.com/goburrow/modbus v0.1.0
+	github.com/goburrow/serial v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/goburrow/modbus"
+)
+
+func bytesToIntArr(b []byte) (result []int, err error) {
+
+	var tmp int
+	for i, value := range b {
+		if i%2 == 0 { // even
+			tmp = int(value) * 256
+		} else { // odd
+			tmp += int(value)
+
+			result = append(result, tmp)
+		}
+	}
+	return
+}
 
 func main() {
-	log.Println("Hello World!")
+	client := modbus.TCPClient("localhost:502")
+
+	results, err := client.ReadHoldingRegisters(0, 4)
+	if err != nil {
+		log.Println(err)
+	}
+
+	result, err := bytesToIntArr(results)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(result)
 }
